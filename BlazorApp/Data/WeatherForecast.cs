e222d859832cebf3fffa8ef35bef614eea02ba40
Behnam Mahdavirad <behnam.mahdavirad@gmail.com>
2020-07-30 14:32:42 +0430

    Test

--- BlazorApp/Data/WeatherForecast.cs
+++ BlazorApp/Data/WeatherForecast.cs
@@ -10,6 +10,6 @@ namespace BlazorApp.Data
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string Summary { get; set; } // Test
     }
 }
